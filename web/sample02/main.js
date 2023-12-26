b16010a597550028bb8033ef22dd187d6ff14074
amami-harhid <amami-thida100@outlook.jp>
2023-12-26 17:13:22 +0900

    sample02

--- web/sample02/main.js
+++ web/sample02/main.js
@@ -1,169 +1,197 @@
-const Sounds = class {
-  static async Wait (milliSecond = Sounds.WAIT_TIME) {
-    return new Promise(resolve => setTimeout(resolve, milliSecond));
-  }
-  static WAIT_TIME () {
-    return 0.05;
-  }
-  static get AudioEngine() {
-    if( Sounds._audioEngine ) {
-      return Sounds._audioEngine;
+/**
+ * p5 で作ったCanvas を使うと
+ * ScratchRenderer にてエラーが起きる
+ * Uncaught (in promise) Error: Could not get WebGL context: this browser or environment may not support WebGL.
+ * 
+ */
+const Character = class {
+    constructor (renderer, name, order, imageUrl, x=0, y=0, scale={x:100,y:100} ) {
+      this.renderer = renderer;
+      this.name = name;
+      this.order = order;
+      this.x = x;
+      this.y = y;
+      this.scale = scale;
+      this.skinId;
+      this.skinSVGIdx = -1;
+      this.skinSVGArr = [];
+      this.color = 0;
+      this.mosaic = 0;
+      this.fisheye = 0;
+      this.drawableID = renderer.createDrawable(this.order);
+      this.loaded = false;
+      if(Array.isArray(imageUrl)){
+        imageUrl.forEach(url=>{
+          this.updateImage(url); 
+        })
+      }else{
+        this.updateImage(imageUrl); 
+      }
     }
-    const audioEngine = new AudioEngine();
-    Sounds._audioEngine = audioEngine;
-    return audioEngine;
-  }
-  static set AudioEngine(obj) {
-    Sounds._audioEngine = obj;
-  }
-
-  constructor() {
-    this._cancel = false;
-  }
-
-  /**
-   * Start sound.
-   */
-  startSound() {
-    this.soundPlayer.play();
-  }
-
-  /**
-   * Start sound until done forEver.
-   */
-  async soundForever() {
-    this._cancel = false;
-    for(;;) {
-      this.soundPlayer.play();
-      await this.soundPlayer.finished(); // 終わるまで待つ
-      if(this._cancel) break;
-      await Sounds.Wait();
+    async svgText(imageUrl) {
+      let svg = await fetch(imageUrl)
+      let txt = await svg.text();
+      return txt;
     }
-  }
+    async updateImage(imageUrl) {
+      //let txt = this.svgText(imageUrl);
+      const _this = this;
+      //var image = new Image();
+      let t = await this.svgText(imageUrl);
+      //txt.then(t=>{
+        let skinId =_this.renderer.createSVGSkin(t);
+        if( this.skinSVGArr.length == 0){
+          this.skinId = skinId;  
+          this.skinSVGIdx = 0;
+        }
+        this.skinSVGArr.push(t);
+        _this.renderer.updateDrawableProperties(_this.drawableID, {
+          skinId: this.skinId,
+          scale: [_this.scale.x, _this.scale.y],
+          color: this.color,
+          mosaic: this.mosaic,
+          fisheye: this.fisheye,
 
-  /**
-   * Start sound until done.
-   */
-  async startSoundUntilDone() {
-    this.soundPlayer.play();
-    await this.soundPlayer.finished(); // 終わるまで待つ
-  }
-
-  /**
-   * Stop immediately without fading out. May cause audible clipping
-   */
-  stopImmediately() {
-    this._cancel = true;
-    this.soundPlayer.stopImmediately();
-  }
-
-  /**
-   * Stop playback after quickly fading out.
-   * if immediately is specified, stop immediately without fading out. May cause audible clipping
-   * @param {boolean} - immediately.
-   */
-  stop(immediately = false) {    
-    this._cancel = true;
-    if(immediately) {
-      this.soundPlayer.stopImmediately();
-    }else{
-      this.soundPlayer.stop();
+        });
+        _this.loaded = true;
+      //});  
     }
-  }
-
-  /**
-   * create sound player.
-   * @param {string} - soundUrl.
-   * @returns {SoundPlayer} - soundPlayer.
-   */
-  async createSoundPlayer(soundUrl) {
-    const data = await this._loadSound(soundUrl);
-    const soundPlayer = await Sounds.AudioEngine.decodeSoundPlayer({data});
-    this.soundPlayer = soundPlayer;
-    const soundEffectChain = Sounds.soundEffectChain;
-    Sounds.soundEffectChain = soundEffectChain;
-//    Sounds.soundEffectChain.set(Sounds.soundEffectChain.pitch.name, 150);
-//    this.soundPlayer.connect(Sounds.soundEffectChain.pan);
-//    this.volume = 0;
-//    this.soundPlayer.connect(Sounds.soundEffectChain.pitch);
-    this.soundPlayer.connect(Sounds.soundEffectChain);
-    // Volume 以外のエフェクトが効かない。どうして？
-    return soundPlayer;
-  }
-
-  /**
-   * volume setter.
-   * @param {number} - volume of sound.
-   */
-  set volume(value) {
-    Sounds.soundEffectChain.set(Sounds.soundEffectChain.volume.name, value);
-//    Sounds.soundEffectChain.update();
-    Sounds.soundEffectChain.volume.update();
-  }
-  /**
-   * volume getter
-   * @returns {number} - volume of sound.
-   */
-  get volume() {
-    const volume = Sounds.soundEffectChain[Sounds.soundEffectChain.volume.name].value;
-    return volume;
-  }
-
-  /**
-   * soundPlayer getter
-   * @returns {SoundPlayer} - _soundPlayer.
-   */
-  get soundPlayer() {
-    return this._soundPlayer;
-  }
-  /**
-   * soundPlayer setter
-   * @param {SoundPlayer} - _soundPlayer.
-   */
-  set soundPlayer(_soundPlayer) {
-    this._soundPlayer = _soundPlayer;
-  }
-  /**
-   * effectChain getter
-   * @returns {EffectChain} - effectChain.
-   */
-  static get soundEffectChain() {
-    if( Sounds._soundEffectChain ) {
-      return Sounds._soundEffectChain;
+    nextCostume() {
+      if( this.skinSVGArr.length > 0) {
+        if( this.skinSVGIdx+1 < this.skinSVGArr.length ) {
+          this.skinSVGIdx += 1;
+        }else{
+          this.skinSVGIdx = 0;
+        }
+        let t = this.skinSVGArr[this.skinSVGIdx ];
+        this.renderer.updateSVGSkin(this.skinId, t);
+      }
     }
-    const soundEffectChain = Sounds.AudioEngine.createEffectChain();
-    return soundEffectChain;
-  }
-  /**
-   * effectChain setter
-   * @param {EffectChain} _effectChain - effectChain.
-   */
-  static set soundEffectChain(_effectChain) {
-    Sounds._soundEffectChain = _effectChain;
-  }
-  /**
-   * load sound data
-   * @param {string} soundUrl - url of sound data.
-   * @returns {Unit8Array} - sound's arayBuffer.
-   */
-  async _loadSound(soundUrl) {
-    let response = await fetch(soundUrl);
-    let buffer = await response.arrayBuffer();
-    let data =  new Uint8Array(buffer);
-    return data;
-  }
+    update() {
+      const _this = this;
+      this.renderer.updateDrawableProperties(this.drawableID, {
+        position: [_this.x, _this.y],
+        scale: [_this.scale.x, _this.scale.y],
+        direction: 90,
+        skinId: _this.skinId,
+        color: _this.color,
+        mosaic: _this.mosaic,
+        fisheye: _this.fisheye,
+      });
+      _this.renderer.draw();
+    }
+    isTouching(_targetCharacter, _callback) {
+      let _targetDrawableId = _targetCharacter.drawableID;
+      if( this.renderer.isTouchingDrawables(_targetDrawableId) ) {
+        if( _callback ){
+          _callback();
+        }
+        return true;
+      }
+      return false;
+    }
+    move() {
 
-  /**
-   * Set the sound's playback rate.
-   * @param {number} value - pitch rate. Default is 1.
-   */
-  set pitch(value = 1) {
-    /*
-    Sounds.soundEffectChain.set(Sounds.soundEffectChain.pitch.name, value);
-    Sounds.soundEffectChain.pitch.value = value;
-    Sounds.soundEffectChain.pitch.update();
-    */
-    this.soundPlayer.setPlaybackRate(value);
-  }
-
+    }
 }
+const Wait = async function (second) {
+  return new Promise(resolve => setTimeout(resolve, second));
+}
+const Chill = '../assets/Chill.wav';
+let sound;
+const moduleStart = async() => {
+
+  scratch_setup();
+  //scratch_drawup();
+  setInterval(scratch_drawup, 1000/30);
+  
+  const sound = new Sounds();
+  await sound.createSoundPlayer(Chill);
+  let _playbackRate = 0.5;
+  sound.pitch = 0.5;
+  sound.volume = 50;
+  setInterval(function(){
+    if( sound.volume == 10 ){
+        sound.volume = 100;
+    }else{
+        sound.volume = 10;
+    }
+  },500);
+  setInterval(function(){
+    if( _playbackRate > 2) {
+      _playbackRate = 0.5;
+    }
+    _playbackRate += 0.1;
+    sound.pitch = _playbackRate;
+  },20);
+  for(;;){
+    await sound.startSoundUntilDone();
+  }
+};
+let canvas;
+const W = innerWidth;//759;//480;//innerWidth;  480*2 = 760
+const H = innerHeight;//569;//360;//innerHeight; 360*2 = 620;
+const CanvasRate = 1;
+let character;
+let character2;
+let renderer;
+
+const scratch_setup = function() {
+  const main = document.getElementById('canvas');
+  const canvas = document.createElement('canvas');
+  main.appendChild(canvas);
+  renderer = new ScratchRenderer(canvas);
+  renderer.setLayerGroupOrdering(['1','2']);
+  renderer.resize( W, H);
+  renderer.setStageSize(-W/2,W/2,-H/2,+H/2);
+  const costumes = ["../assets/cat.svg","../assets/cat2.svg"];
+  character = new Character(renderer, "cat", "1", costumes, 0, 0, {x:500, y:500});    
+  character2 = new Character(renderer, "cat2", "2", costumes, 0, 100, {x:-150, y:150});
+  
+}
+const Power = 50;
+let speed = Power;
+let chara1RightSpeed = 20;
+let chara2RightSpeed = -20;
+let chara1OutofChara2 = true;
+const scratch_drawup = function () {
+
+  if(character.isTouching(character2)) {
+    renderer.setBackgroundColor(1,1,1,1);
+    character.fisheye = 100;
+    if(chara1OutofChara2 === true) {
+      chara1RightSpeed *= -1;
+      character.scale.x *= -1;  
+      chara2RightSpeed *= -1;
+      character2.scale.x *= -1;
+      speed = Math.random()*Power;
+    }
+    chara1OutofChara2 = false;
+  }else{
+    chara1OutofChara2 = true;
+    renderer.setBackgroundColor(0.6,0.1,0.1, 1);
+    character.fisheye = 0;
+  }
+  character.update();
+  character2.update();
+  character.x += chara1RightSpeed;
+  if( character.x > W/2 || character.x < -W/2) {
+    chara1RightSpeed *= -1;
+    character.scale.x *= -1;
+  }
+  character2.y += speed;
+  speed += -2;
+  if( character2.y < -H/2 ) {
+    speed = Power;
+  }
+  character2.x += chara2RightSpeed;
+  if( character2.x > W/2 || character2.x < -W/2) {
+    chara2RightSpeed *= -1;
+    character2.scale.x *= -1;
+  }
+  character.nextCostume();
+  character2.nextCostume();
+
+  //requestAnimationFrame(scratch_drawup);
+}
+
