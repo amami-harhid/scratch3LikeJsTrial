06c4d96f8c90e46fc4c3072c313d4d4e883ecccb
amami-harhid <amami-thida100@outlook.jp>
2023-12-31 11:19:47 +0900

    sample02

--- web/sample01/scripts.js
+++ web/sample01/scripts.js
@@ -1,36 +1,18 @@
 const draw = function(render){
-    render.renderer.draw();
+  render.renderer.draw();
 }
+
 LS.start = async function (){
-    const LayerGroup = ["1", "2"];
-    const render = new LS.Render(LayerGroup);
-    const stage = new LS.Stage(render, "stage", LayerGroup[0]);
-    await stage.backdrops.loadImage('jurassic', '../assets/Jurassic.svg');
-    stage.scale = {x:100,y:100};
-    const spriteA = new LS.Sprite(render, "spriteA",  LayerGroup[1]);
-    spriteA.scale = {x:80,y:80};
-    await spriteA.costumes.loadImage('catA', '../assets/cat.svg');    
-    stage.addSprite(spriteA)
+  const LayerGroup = ["1", "2"];
+  const render = new LS.Render(LayerGroup);
+  const stage = new LS.Stage(render, "stage", LayerGroup[0]);
+  await stage.backdrops.loadImage('jurassic', '../assets/Jurassic.svg');
+  stage.scale = {x:100,y:100};
+  const wait_time = 1000/30;
+  for(;;){
     stage.update();
-
-    const times = Array(40).fill();
-    const wait_time = 1000/30;
-    for(;;){
-
-      for(const t in times){
-        spriteA.setScale(spriteA.scale.x + 5);
-        stage.update();
-        draw(render);
-        await LS.Utils.wait(wait_time);
-      }
-      for(const t in times){
-        spriteA.setScale(spriteA.scale.x - 5);
-        stage.update();
-        draw(render);
-        await LS.Utils.wait(wait_time);
-      }
-
-      await LS.Utils.wait(wait_time);
-    }
-        
-  };
+    draw(render);
+    await LS.Utils.wait(wait_time);
+  }
+      
+};
