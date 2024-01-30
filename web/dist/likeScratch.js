c4fa0b4d60bba3309591e5b4cffcd976f86d92aa
amami-harhid <amami-thida100@outlook.jp>
2024-01-30 18:35:05 +0900

    変数 位置試行中

--- web/dist/likeScratch.js
+++ web/dist/likeScratch.js
@@ -203,11 +203,36 @@ const Process = class {
         return this._render.stageHeight;
     }
 
-    get mousePosition () {
+    toScratchPosition(x, y) {
+        // Base position -> canvas 
+        const rate = this.renderRate;
+        const _x = x * rate.x;
+        const _y = y * rate.y;
+        return {x: _x, y: _y};
+    }
+
+    toActualPosition( x, y ) {
+
+        const rate = this.renderRate;
+        const _x = x / rate.x;
+        const _y = y / rate.y;
+        return {x: _x, y: _y};
+
+    }
+
+    getRenderRate() {
+        return this.renderRate;        
+    }
+
+    get renderRate() {
         const _rateX = this._render.stageWidth / this.canvas.width;
         const _rateY = this._render.stageHeight / this.canvas.height;
-        const _mouseX = (this.stage.mouse.x - this.canvas.width/2 ) * _rateX;
-        const _mouseY = (this.canvas.height/2 - this.stage.mouse.y) * _rateY;
+        return {x: _rateX, y:_rateY};
+    }
+    get mousePosition () {
+        const rate = this.renderRate;
+        const _mouseX = (this.stage.mouse.x - this.canvas.width/2 ) * rate.x;
+        const _mouseY = (this.canvas.height/2 - this.stage.mouse.y) * rate.y;
         return {x: _mouseX, y: _mouseY};
     }
 
@@ -39312,116 +39337,260 @@ module.exports = Stage;
 /* 133 */
 /***/ (function(module, exports, __webpack_require__) {
 
+const Process = __webpack_require__(0);
 const Utils = __webpack_require__(3);
 
 const Variable = class {
-    constructor(name, label, no=1, scale=1) {
-        this.name = name;
-        if( label == undefined) {
-            this.label = name;
-        }else{
-            this.label = label;
-        }
+    constructor(label, no=1, scale) {
+        this._label = label;
         this.id = Utils.generateUUID();
-        this.value = '';
+        this.x = 0;
+        this.y = 0;
+        this.translateX = 0;
+        this.translateY = 0;
+        this._value = '';
         this.stageMonitorContainer = null;
         this.monitorLabel = null;
         this.monitorLabel = null;
         this.no = no
-        this.scale = scale;
-        this.init();
+        this._scale = (scale==undefined)? 1 : scale;
+        this._maxSize = 5;
+        const target = this.init();
+        target.setAttribute('scratch-scale', this._scale);
+        this.interact(target, scale);
     }
 
     init() {
+        const process = Process.default;
+        const renderRate = process.renderRate;
 
         const getElementById = (id) => document.getElementById(id)
         const createElement = (node) => document.createElement(node); 
         const _stageCanvasWrapper = getElementById('stageCanvasWrapper');
     
+        const uid = this.id;
+
         const stageMonitorContainer = createElement('div');
         this.stageMonitorContainer = stageMonitorContainer;
         stageMonitorContainer.classList.add('monitor_monitor-container');
         stageMonitorContainer.classList.add('dragged-item');
         stageMonitorContainer.classList.add('dragTarget');
-        stageMonitorContainer.id = `stageMonitorContainer_${this.id}`
+        stageMonitorContainer.id = `stageMonitorContainer_${uid}`
         stageMonitorContainer.setAttribute("style","touch-action: none;");
     
         _stageCanvasWrapper.appendChild(stageMonitorContainer);
     
         const defaultMonitor = createElement('div');
+        defaultMonitor.id = `defaultMonitor_${uid}`;
         defaultMonitor.top = "100px";
         defaultMonitor.left = "100px";
         defaultMonitor.classList.add('monitor_default-monitor');
         stageMonitorContainer.appendChild(defaultMonitor);
         
         const monitorRaw = createElement('div');
+        monitorRaw.id = `monitorRaw_${uid}`;
         monitorRaw.classList.add('monitor_row');
         defaultMonitor.appendChild(monitorRaw);
     
         const monitorLabel = createElement('div');
+        monitorLabel.id = `monitorLabel_${uid}`;
         this.monitorLabel = monitorLabel;
 
         monitorLabel.classList.add('monitor_label');
         monitorRaw.appendChild(monitorLabel);
-        monitorLabel.innerHTML  = `${this.label}`;
+        monitorLabel.innerHTML  = `${this._label}`;
 
         const monitorValue = createElement('div');
+        monitorValue.id = `monitorValue_${uid}`;
         this.monitorValue = monitorValue;
 
         monitorValue.classList.add('monitor_value');
         monitorRaw.appendChild(monitorValue);
-        monitorValue.innerHTML  = '    '; // 初期値スペース４文字分
+        monitorValue.innerHTML  = '     '; // 初期値スペース５文字分
     
+        const balloon = createElement('div');
+        this.balloon = balloon;
+        balloon.classList.add('monitor_balloon');
+        balloon.innerHTML  = '---';
+        stageMonitorContainer.appendChild(balloon);
+
+
         // canvas左上基準で位置決めする
         // すでに存在する Variable の表示をさけて表示させたい。
         // canvas のサイズ変更に合わせて top, left を変更したい。
-        const top = 10;
-        const left = 10;
-        const scale = this.scale;
-        stageMonitorContainer.style.top = `${top}px`;// (canvasClientRect.top+50) +"px";
-        stageMonitorContainer.style.left =`${left}px` ;// (canvasClientRect.left+50) +"px";
-        stageMonitorContainer.style.transform = `scale(${scale})`
-        interact(stageMonitorContainer).styleCursor(false)
-        interact(stageMonitorContainer).draggable({
-          manualStart: false,
-          listeners: {
-            start(event) {
-              event.target.classList.add('dragging');
-            },
-            move(event) {
-              pos.x += event.dx
-              pos.y += event.dy
-              if( !event.target.classList.contains('dragging')) {
-                //event.target.classList.add('dragging');
-              }
-              event.target.style.transform = `translate(${pos.x}px, ${pos.y}px) scale(${scale})`
-            },
-            end(event) {
-              event.target.classList.remove('dragging');
-            }
-          },
-        })
+        //const top = 0;
+        //const left = 0;
+        const scale = {x : this._scale / renderRate.x , y: this._scale / renderRate.y }
+        stageMonitorContainer.style.top = 0; //`${top}px`;// (canvasClientRect.top+50) +"px";
+        stageMonitorContainer.style.left = 0; //`${left}px` ;// (canvasClientRect.left+50) +"px";
+        stageMonitorContainer.style.transform = `scale(${scale.x}, ${scale.y})`;
+        return stageMonitorContainer;
+    }
+    interact(target , scale) {
+        const me = this;
+        const process = Process.default;
+        interact(target).styleCursor(false)
+        interact(target).draggable({
+            inertia: true,
+
+            modifiers: [
+                interact.modifiers.restrictRect({
+                    restriction: 'parent',
+                    endOnly: true
+                }),
+            ],
+
+            listeners: {
+
+                start(event) {
+                  event.target.classList.add('dragging');
+                },
+
+                move(event) {
+                    const renderRate = process.renderRate;
+                    var target = event.target;
+
+                    // 移動量 や位置を ScratchX, ScratchY に変換し
+                    // data-x, data-y へ保存
+                    // 使うときには、表示率を使って 実際の大きさへ直して使う！
+                    // うまくうごかない！！
+
+                    // data-x, data-y は Scratch Base Position
+
+                    const dxScratchPosition = process.toScratchPosition(  event.dx, event.dy );
+
+                    // keep the dragged position in the data-x/data-y attributes
+                    const scratchX = (parseFloat(target.getAttribute('scratch-x')) || 0) + dxScratchPosition.x;
+                    const scratchY = (parseFloat(target.getAttribute('scratch-y')) || 0) + dxScratchPosition.y;
+                    // update the posiion attributes
+                    target.setAttribute('scratch-x', scratchX);
+                    target.setAttribute('scratch-y', scratchY);
+                    const scale = me._scale; //(parseFloat(target.getAttribute('scratch-scale')) || 1);
+                    const actualScale = {x: scale /  renderRate.x , y: scale / renderRate.y };
+                    const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+                    const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+                    target.setAttribute('data-x', x);
+                    target.setAttribute('data-y', y);
+
+                    // translate the element
             
+                    if(scale == undefined) {
+                        target.style.transform = `translate( ${x}px, ${y}px )`;
+                    }else{
+                        target.setAttribute('scale-x', actualScale.x)
+                        target.setAttribute('scale-y', actualScale.y)
+                        target.style.transform = `scale( ${actualScale.x, actualScale.y} ) translate( ${x/actualScale.x}px, ${y/actualScale.y}px )`;
+                    }
+
+                    const thisId = target.id;
+                    const balloon = document.querySelectorAll(`#${thisId} .monitor_balloon`);
+
+                    if( balloon && balloon.length>0) {
+                        const _balloon = balloon[0];
+                        const stageCanvas = document.getElementById('stageCanvasWrapper');
+                        const stageRect = stageCanvas.getBoundingClientRect();
+                        const targetRect = target.getBoundingClientRect();
+                        const top = targetRect.top - stageRect.top;
+                        const left = targetRect.left - stageRect.left;
+                        //console.log(`top= ${top}, left=${left}`);
+                        const baloonScratchPosition = process.toScratchPosition(left, top);
+                        _balloon.innerHTML = `(${Math.ceil(scratchX)}, ${Math.ceil(scratchY)})`;
+                    }
+                },
+
+                end(event) {
+                    event.target.classList.remove('dragging');
+                    console.log(`me.translateX= ${me.translateX}, me.translateY= ${me.translateX}`);
+
+                }
+
+            },
+        })         
+
     }
     hide() {
         this.stageMonitorContainer.style.display = 'none';
     }
     show() {
         this.stageMonitorContainer.style.display = '';
+    }
+    get size() {
+        const rect = this.stageMonitorContainer.getBoundingClientRect();
+        const w = rect.width;
+        const h = rect.height;
+        return {w:w, h:h};
+    }
+    setPosition( position) {
+        this.x = position.x;
+        this.y = position.y;
+        const target = this.stageMonitorContainer;
+        target.setAttribute('scratch-x', this.x)
+        target.setAttribute('scratch-y', this.y)
+        this.resize();
+    }
+    resize(scaleChange) {
+        const target = this.stageMonitorContainer;
+        const _rect = target.getBoundingClientRect();
+        console.log('_rect.left=',_rect.left);
+        const process = Process.default;
+        const renderRate = process.getRenderRate();
+        const actualPosition = process.toActualPosition(this.x, this.y);
+        const left = actualPosition.x;
+        const top = actualPosition.y;
+        const scaleX = this._scale / renderRate.x;
+        const scaleY = this._scale / renderRate.y;
 
+        target.style.top = 0; //`${top}px`;// (canvasClientRect.top+50) +"px";
+        target.style.left = 0; //`${left}px` ;// (canvasClientRect.left+50) +"px";
+        const dataX = parseFloat(target.getAttribute('scratch-x')) || 0;
+        const dataY = parseFloat(target.getAttribute('scratch-y')) || 0;
+        const dActualPosition = process.toActualPosition(dataX, dataY);
+
+/* 
+        const style = window.getComputedStyle(this.stageMonitorContainer)
+        const matrix = style.transform || style.mozTransform
+        const matrixValues = matrix.match(/matrix.*\((.+)\)/)[1].split(', ');
+
+        const target = this.stageMonitorContainer;
+        const dataX = parseFloat(target.getAttribute('data-x')) || 0;
+        const dataY = parseFloat(target.getAttribute('data-y')) || 0;
+
+        const _x = dataX * renderRate.x;
+        const _y = dataY * renderRate.y;
+*/
+        target.setAttribute('data-x', dActualPosition.x)
+        target.setAttribute('data-y', dActualPosition.y)
+//        target.style.transform = `translate(${dActualPosition.x}px, ${dActualPosition.y}px) scale(${scaleX, scaleY})`;
+        if(scaleChange) {
+            target.setAttribute('scale-x', scaleX)
+            target.setAttribute('scale-y', scaleY)
+            target.style.transform = `scale(${scaleX}, ${scaleY}) translate(${dActualPosition.x}px, ${dActualPosition.y}px)`;
+        }else{
+            const _scaleX = parseFloat(target.getAttribute('scale-x')) || this._scale;
+            const _scaleY = parseFloat(target.getAttribute('scale-y')) || this._scale;
+            target.style.transform = `scale(${_scaleX}, ${_scaleY}) translate(${dActualPosition.x}px, ${dActualPosition.y}px)`;
+        }
     }
-    changeLabel(label) {
-        this.label = label;
-        this.monitorLabel.innerHTML = `${this.label}`;
+
+
+    set balloonText ( text ) {
+        this.balloon.innerHTML = text;
     }
-    setValue(value, maxSize=10) {
-        this.value = value;
-        const _maxSize = maxSize;
-        if(Utils.isNumber(this.value)) {
-            const str = String(this.value);
+    set label (_label) {
+        this._label = _label;
+        this.monitorLabel.innerHTML = `${this._label}`;
+    }
+    set maxSize (maxSize) {
+        this._maxSize =  maxSize;
+    }
+    set value(value) {
+        this._value = value;
+        const _maxSize = this._maxSize;
+        if(Utils.isNumber(this._value)) {
+            const str = String(this._value);
             this.monitorValue.innerHTML = str.padStart(_maxSize, ' ');
         }else{
-            this.monitorValue.innerHTML = this.value.padEnd(_maxSize, ' ');
+            this.monitorValue.innerHTML = this._value.padEnd(_maxSize, ' ');
         }
     }
 }
--- web/testVariables/scripts.js
+++ web/testVariables/scripts.js
@@ -2,28 +2,75 @@ const Variables = class {
 
     constructor() {
         this.map = new Map();
+        this.v = {};
+        const me = this;
+        let timeoutId = 0;
+        const delay = 100;
+        window.addEventListener('resize', async function(){
+            
+            clearTimeout(timeoutId);
+            
+            timeoutId = setTimeout(async function(){
+                //console.log('resize')
+                const r1 = P.getRenderRate();
+                //console.log('resize(1)', r1);
+                await P.wait(100);
+                const r2 = P.getRenderRate();
+                //console.log('resize(2)', r2);
+                const keys = Array.from(me.map.keys());
+                keys.map((k)=>{
+                    const v = me.map.get(k);
+                    v.resize();
+                });
 
+            }, delay);
+        }, false);
     }
-
-    addVariable(variable) {
-        if(!this.map.has(variable.name)) {
-            this.map.set(variable.name, variable);
+    add(label, scale) {
+        if(!this.map.has(label)) {
+            const length = this.map.size;
+            const v = new P.Variable(label, length+1,  scale);
+            this.map.set(label, v);
+            this.v[label] = v;
         }
     }
 
-    changeLabel(name, label) {
-        if(this.map.has(name)) {
-            const variable = this.map.get(name);
-            variable.changeLabel(label);
+    getVariable(label) {
+        if(this.map.has(label)) {
+            const v = this.map.get(label);
+            return v;
         }
+        return null;
     }
-    setValue(name, value, maxSize=10) {
-        if(this.map.has(name)) {
-            const variable = this.map.get(name);
-            variable.setValue(value, maxSize);
-        }
+    automatic () {
+
+        const mapKeys = this.map.keys();
+        const keys = [...mapKeys];
+        const sortKeys = keys.sort(function(a, b){
+            a.no < b.no;
+        })
+        console.log(sortKeys);
+
+        let prevPosition;
+        sortKeys.map((key,idx)=>{
+            const v = this.map.get(key);
+            const size = v.size;
+            console.log(size);
+            if( prevPosition == undefined) {
+                const x = 10;
+                const y = 10;
+                prevPosition = {x: x, y: y};
+                v.setPosition( {x: x, y: y} );
+            }else{
+                const x = 10;
+                const y = prevPosition.y + size.h;
+                prevPosition = {x: x, y: y};
+                v.setPosition( {x: x, y: y} );
+            }
+        })
 
     }
+
 }
 P.preload = async function() {
     this.loadImage('../assets/Jurassic.svg','Jurassic');
@@ -31,16 +78,50 @@ P.preload = async function() {
 
 P.prepare = async function() {
     const a = new P.Stage();
-    P.test01 = new P.Variable('test01','Counter', 1, 2);
+    P.variables = new Variables();
+    P.variables.add('Counter');
+    P.variables.add('マウス');
+    P.variables.v.Counter.setPosition( {x: 40, y:15} )
+    P.variables.v.マウス.setPosition( {x: 330, y:44} )
+
+    P.variables.v.Counter.value = 0;
+    P.variables.v.マウス.value = 'xxxxx';
+    //P.variables.automatic();
+/*
+    P.test01 = new P.Variable('Counter', undefined, 1, 2);
     P.test01.changeLabel('Counter');
-    P.test02 = new P.Variable('test02', 'マウス', 2, 2);
+    P.test02 = new P.Variable('マウス', undefined, 2, 2);
     P.test02.changeLabel('マウス');
-
-
-    
+*/  
 }
 
 P.setting = async function() {
+
+    console.log('P._render.stageWidth', P._render.stageWidth)
+    console.log('P._render.stageHeight', P._render.stageHeight)
+    console.log('P.canvas.width', P.canvas.width)
+    console.log('P.canvas.height', P.canvas.height)
+
+    const dragTargets = document.querySelectorAll('.dragTarget');
+    console.log('dragTargets',dragTargets)
+    Array.from(dragTargets).map(d=>{
+        d.addEventListener('mouseover', function(){
+            const id = d.id;
+            const balloons = document.querySelectorAll(`#${id} .monitor_balloon`);
+            Array.from(balloons).map(b=>{
+                b.style.display = 'inline';
+            });
+        })
+        d.addEventListener('mouseout', function(){
+            const id = d.id;
+            const balloons = document.querySelectorAll(`#${id} .monitor_balloon`);
+            Array.from(balloons).map(b=>{
+                b.style.display = 'none';
+            });
+        })
+      
+    })
+
 
     P.stage.whenFlag( async function() {
         this.addImage( P.images.Jurassic );
@@ -48,7 +129,7 @@ P.setting = async function() {
         let counter = 0;
         for(;;) {
     
-            P.test01.setValue(counter,3);
+            P.variables.v.Counter.value= counter;
     
             await P.wait(1000);
             counter += 1;
@@ -56,15 +137,29 @@ P.setting = async function() {
     });
 
     P.stage.whenFlag( async function() {
-        console.log('whenFlag')
         let counter = 0;
         for(;;) {
             const position = P.mousePosition;
-            P.test02.setValue(` ${Math.ceil(position.x)} , ${Math.ceil(position.y)}`);
+            P.variables.v.マウス.value = ` ${Math.ceil(position.x)} , ${Math.ceil(position.y)}`;
     
             await P.wait(33);
             counter += 1;
         }
     });
 
+}
+
+function dragMoveListener (event) {
+
+    var target = event.target
+    // keep the dragged position in the data-x/data-y attributes
+    var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+    var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+  
+    // translate the element
+    target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+  
+    // update the posiion attributes
+    target.setAttribute('data-x', x)
+    target.setAttribute('data-y', y)
 }
